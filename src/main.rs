8eb94460ffb10976aeda78f8d969d2de31673c2b
c-node <chuckhead7@gmail.com>
2025-06-05 17:38:42 -0400

    impl fq

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 // Let's use this as a reference 
 // https://github.com/HorizenLabs/poseidon2/tree/main/plain_implementations
 mod scalar_field;
+use scalar_field::Fq;
 
 fn main() {
     println!("Hello, world!");
 }
-
