a1ded78ee12b73e4a593133d2b2fec76418f8084
Nuke <nuke-web3@proton.me>
2025-06-06 15:03:18 -0600

    try bypass issues

--- src/fields/bn256.rs
+++ src/fields/bn256.rs
@@ -16,11 +16,33 @@ use crypto_bigint::{Encoding, Uint, impl_modulus, modular::constant_mod::Residue
 /// Type alias to ensure no conflicts with SP1 U256 definition
 pub type U256Field = Uint<4>;
 
-impl_modulus!(
-    ModulusBN254,
-    U256Field,
-    "30644E72E131A029B85045B68181585D2833E84879B9709143E1F593F0000001"
-);
+// Expansion for:
+// impl_modulus!(
+//     ModulusBN254,
+//     U256Field,
+//     "30644E72E131A029B85045B68181585D2833E84879B9709143E1F593F0000001"
+// );
+//
+// Required as internally from_be_hex(...) is fucked by SP1
+#[derive(Clone,Copy,Debug,Default,Eq,PartialEq)]
+pub struct ModulusBN254{}
+
+impl <const DLIMBS:usize>crypto_bigint::modular::constant_mod::ResidueParams<{
+    <U256Field>::LIMBS
+}>for ModulusBN254 where U256Field:crypto_bigint::ConcatMixed<MixedOutput = crypto_bigint::Uint<DLIMBS>>,{
+    const LIMBS:usize =  <U256Field>::LIMBS;
+    // BE Bytes, Hex = 0x30644E72E131A029B85045B68181585D2833E84879B9709143E1F593F0000001
+    const MODULUS: U256Field = U256Field::from_be_slice(&[
+        0x30, 0x64, 0x4E, 0x72, 0xE1, 0x31, 0xA0, 0x29,
+        0xB8, 0x50, 0x45, 0xB6, 0x81, 0x81, 0x58, 0x5D,
+        0x28, 0x33, 0xE8, 0x48, 0x79, 0xB9, 0x70, 0x91,
+        0x43, 0xE1, 0xF5, 0x93, 0xF0, 0x00, 0x00, 0x01,
+    ]);
+    const R:U256Field = crypto_bigint::Uint::MAX.const_rem(&Self::MODULUS).0.wrapping_add(&crypto_bigint::Uint::ONE);
+    const R2:U256Field = crypto_bigint::Uint::const_rem_wide(Self::R.square_wide(), &Self::MODULUS).0;
+    const MOD_NEG_INV:crypto_bigint::Limb = crypto_bigint::Limb(crypto_bigint::Word::MIN.wrapping_sub(Self::MODULUS.inv_mod2k_vartime(crypto_bigint::Word::BITS as usize).as_limbs()[0].0,),);
+    const R3:U256Field = crypto_bigint::modular::montgomery_reduction(&Self::R2.square_wide(), &Self::MODULUS,Self::MOD_NEG_INV,);
+}
 
 pub type FpBN256 = Residue<ModulusBN254, 4>; // LIMBS = 4 for max 256bit field
 
